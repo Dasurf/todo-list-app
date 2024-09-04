9f9aa1becc10935000d3af4bd1a54f5c30502210
DASURF <damilareyusuf.dy@gmail.com>
2024-09-04 20:28:36 +0100

    Fixed bugs and mdee text input not accept empty field, also used local storage to save states of the todo list

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,74 +1,101 @@
 import React, {useState} from "react";
 
 export default function Todos({todos, addTodo}) {
-    const [input, setInput] = useState("");
-
-    function handleChange(event) {        
-        const {value} = event.target;
-        
-        setInput(value);
-    }
-    console.log(input);
-
-    function handleClick(event) 
-    {
-        event.preventDefault();
-        console.log("handleClick called");
-        addTodo(prev => {
-            return input === "" ? prev : [...prev, input];
-        })
-
-        setInput("");
-        // console.log(todos);
-    }
-
-    function removeTodo(index) {
-        addTodo(prev => {
-            const newTodos = [...prev];
-            newTodos.splice(index, 1);
-            
-            return newTodos;
-        })
-    }
+	const [input, setInput] = useState("");
 
 
+	function handleChange(event) {        
+		const {value} = event.target;
+
+		setInput(value);
+	}
+	console.log(input);
+
+	function handleClick(event) {
+	
+		event.preventDefault();
+		console.log("handleClick called");
+
+		addTodo(prev => {
+			if (input === "") {
+				return [...prev]
+			}
+			else {
+				return [
+				...prev,
+				{
+					id: crypto.randomUUID(),
+					title: input,
+					completed: false
+				}
+			]
+			} 
+		})
+
+		setInput("");
+		// console.log(todos);
+	}
+
+	function toggleTodo(id, completed) {
+		addTodo(prev => {
+			return prev.map(todo => {
+				if (todo.id === id) {
+				   return {...todo, completed};
+				} 
+				
+				return todo;
+			})
+		})
+	}
 
 
-    return (
-        <div className="main--container">
-            <h2>New Item</h2>
-            <form className="input--form">
-                <input
-                    className="text--input"
-                    name="textInput"
-                    type="text"
-                    placeholder="Enter Todo"
-                    onChange={handleChange}
-                    value={input}
-                />
-                <button onClick={handleClick}>Add</button>
-            </form>
-            <h2>Todos</h2>
-            {
-                todos.length !== 0
-                ?
-                todos.map((todo, index) => {
-                    return (
-                        <div key={index} className="output">
-                            <input
-                                type="checkbox"
-                                name="completed"
-                            />
-                            <label htmlFor="completed">{todo}</label>
-                            <button className="delete--btn" onClick={() => {
-                                removeTodo(index);
-                            }}>Delete</button>
-                        </div>
-                    )
-                })
-                :
-                <h4 className="no--todos">No Todos</h4>
-            }
-        </div>
-    )
+	function removeTodo(index) {
+		addTodo(prev => {
+			const newTodos = [...prev];
+			newTodos.splice(index, 1);
+			
+			return newTodos;
+		})
+	}
+	
+	return (
+		<div className="main--container">
+			<h2>New Item</h2>
+			<form className="input--form">
+				<input
+					id="item"
+					className="text--input"
+					name="textInput"
+					type="text"
+					placeholder="Enter Todo"
+					onChange={handleChange}
+					value={input}
+				/>
+				<button onClick={handleClick}>Add</button>
+			</form>
+			<h2>Todos</h2>
+			{
+				todos.length !== 0
+				?
+				todos.map((todo, index) => {
+					return (
+						<div key={todo.id} className="output">
+							<input
+								type="checkbox"
+								name="completed"
+								onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+								checked={todo.completed}
+							/>
+							<label htmlFor="completed">{todo.title}</label>
+							<button className="delete--btn" onClick={() => {
+								removeTodo(index);
+							}}>Delete</button>
+						</div>
+					)
+				})
+				:
+				<h4 className="no--todos">No Todos</h4>
+			}
+		</div>
+	)
 }
--- src/components/WriteTodo.jsx
+++ src/components/WriteTodo.jsx
@@ -1,8 +1,12 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Todos from "./Todos";
 
 export default function WriteTodo() {
-    const [todos, setTodos] = useState([]);
+    const [todos, setTodos] = useState(JSON.parse(localStorage.getItem("todos")) || []);
+
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
 
     return (
         <section>
